2774db2564e0e0b977246c2011de3ee663417ded
Ogu <8559823-Shogu@users.noreply.gitlab.com>
2026-06-18 14:59:14 +0000

    Modifier config.fish

--- Fichiers de configuration/Logiciels/Fish/config.fish
+++ Fichiers de configuration/Logiciels/Fish/config.fish
@@ -6,9 +6,11 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # === Alias Editeurs ===
 alias vim='micro'
 alias vi='micro'
-alias gedit='gnome-text-editor'
+alias edit='micro'
 alias nano='micro'
 alias notepad='gnome-text-editor'
+alias gedit='gnome-text-editor'
+
 
 # === Alias Système ===
 alias rm='rm -I'
