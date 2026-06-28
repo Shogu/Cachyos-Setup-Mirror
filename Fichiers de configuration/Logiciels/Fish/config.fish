cbf19bb35793e9d554aa529c046a5ffdbe8b6bcc
Ogu <8559823-Shogu@users.noreply.gitlab.com>
2026-06-28 15:50:40 +0000

    Modifier config.fish

--- Fichiers de configuration/Logiciels/Fish/config.fish
+++ Fichiers de configuration/Logiciels/Fish/config.fish
@@ -28,7 +28,7 @@ alias aur='shelly aur install'
 alias aursearch='shelly aur search'
 alias aurremove='shelly aur remove --opt-deps'
 alias aurlist='shelly aur list'
-alias upgrade 'ptyxis --maximize --standalone -e fish -c '\''set_color 3584e4; echo "╔══════════════════════════════════════════════╗"; echo "║  󰮯  MISE À JOUR SHELLY                      ║"; echo "╚══════════════════════════════════════════════╝"; set_color normal; echo; shelly upgrade; and shelly aur upgrade; echo; read'\'''
+alias upgrade='ptyxis --maximize --standalone -e fish -c '\''set_color 3584e4; echo "╔══════════════════════════════════════════════╗"; echo "║  󰮯  MISE À JOUR SHELLY                      ║"; echo "╚══════════════════════════════════════════════╝"; set_color normal; echo; shelly upgrade; and shelly aur upgrade; echo; read'\'''
 
 # === Alias Fish ===
 alias sourcefish='source ~/.config/fish/config.fish'
