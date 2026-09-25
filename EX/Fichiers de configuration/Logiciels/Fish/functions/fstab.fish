546c98645222b71d46ffc11fa3a19d12d907fa72
Ogu <trevisiolugo@gmail.com>
2026-09-25 21:43:36 +0200

    Mise à jour

--- EX/Fichiers de configuration/Logiciels/Fish/functions/fstab.fish
+++
@@ -1,7 +0,0 @@
-function fstab
-    clear
-    echo "/etc/fstab"
-    echo
-    sudo bat --language=fstab --paging=never --style=plain /etc/fstab
-    echo
-end
--- EX/Fichiers de configuration/Logiciels/Fish/functions/mkinitcpio.fish
+++
@@ -1,7 +0,0 @@
-function mkinitcpio
-    clear
-    echo "/etc/mkinitcpio.conf"
-    echo
-    sudo bat --language=ini --paging=never --style=plain /etc/mkinitcpio.conf
-    echo
-end
